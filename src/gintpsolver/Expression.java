9b1ac3a07b416a9915b30ed3163a9ef693ecc289
xavierwoo <xavier.woo@gmail.com>
2015-04-26 09:33:00 +0800

    add abstract method. the method require return several moves based on the change value

--- src/gintpsolver/Expression.java
+++ src/gintpsolver/Expression.java
@@ -19,9 +19,22 @@ public abstract class Expression {
     abstract protected Move find_dec_mv();
 
     /**
+     * Find moves that can decrease the value more than delta
+     * @param delta the change value
+     * @return
+     */
+    abstract protected ArrayList<Move> find_dec_mv(double delta);
+
+    /**
      *
      * @return A random move that can increase the expression value
      */
     abstract protected Move find_inc_mv();
 
+    /**
+     * Find moves that can increase the value more than delta;
+     * @param delta the change value
+     * @return moves found
+     */
+    abstract protected ArrayList<Move> find_inc_mv(double delta);
 }
